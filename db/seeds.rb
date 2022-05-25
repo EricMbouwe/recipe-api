b50c490f2a4813e3a03ea897a100159e259bc00c
ericmbouwe <ericmbouwe@yahoo.fr>
2022-05-25 14:36:49 +0100

    Fix json seed deployement

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,44 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Recipe.destroy_all
-# Ingredient.destroy_all
-# Tag.destroy_all
+Recipe.destroy_all
+Ingredient.destroy_all
+Tag.destroy_all
 
 recipes_records = JSON.parse(File.read(Rails.root.join('db/json/recipes-fr.json')))
 len = recipes_records.size
 p len
 
-# recipes_records.each_with_index do |record, index|
+recipes_records.each_with_index do |record, index|
 
-#     puts "Seeding database...(#{index+1}/#{len})"
+    puts "Seeding database...(#{index+1}/#{len})"
 
-#     ingredient_list = record['ingredients']
-#     tag_list = record['tags']
+    ingredient_list = record['ingredients']
+    tag_list = record['tags']
 
-    # recipe = Recipe.create(
-    #     rate: record['rate'],
-    #     author_tip: record['author_tip'],
-    #     budget: record['budget'],
-    #     prep_time: record['prep_time'],
-    #     name: record['name'],
-    #     author: record['author'],
-    #     difficulty: record['difficulty'],
-    #     people_quantity: record['people_quantity'],
-    #     cook_time: record['cook_time'],
-    #     total_time: record['total_time'],
-    #     image: record['image'],
-    #     nb_comments: record['nb_comments']
-    # )
+    recipe = Recipe.create(
+        rate: record['rate'],
+        author_tip: record['author_tip'],
+        budget: record['budget'],
+        prep_time: record['prep_time'],
+        name: record['name'],
+        author: record['author'],
+        difficulty: record['difficulty'],
+        people_quantity: record['people_quantity'],
+        cook_time: record['cook_time'],
+        total_time: record['total_time'],
+        image: record['image'],
+        nb_comments: record['nb_comments']
+    )
 
-#     ingredient_list.each do |ingredient|
-#         recipe.ingredients.create(name: ingredient)
-#     end
+    ingredient_list.each do |ingredient|
+        recipe.ingredients.create(name: ingredient)
+    end
 
-#     tag_list.each do |tag|
-#         recipe.tags.create(name: tag)
-#     end
+    tag_list.each do |tag|
+        recipe.tags.create(name: tag)
+    end
 
-# end
+end
 
 puts 'Seed complete!'
